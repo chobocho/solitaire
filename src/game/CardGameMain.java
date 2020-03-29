14ad59e1b6f42a34bd947e16f1184f35c1176918
chobocho <chobocho99@gmail.com>
2020-03-29 13:58:57 +0900

    [Patch] Update version

--- src/game/CardGameMain.java
+++ src/game/CardGameMain.java
@@ -9,7 +9,7 @@ import java.awt.*;
 
 public class CardGameMain extends JFrame {
     String TAG = "CardGameMain";
-    String Version = "V0.1105.TC4";
+    String Version = "V0.1105.TC5";
     JLabel statusbar;
 
     public CardGameMain() {
